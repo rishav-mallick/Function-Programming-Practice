aa61236469bb17c4f073ba2630442f6f6e160035
rishav.mallick@westpac.com.au <rishav.mallick@westpac.com.au>
2022-10-21 00:20:24 +0530

    initial commit for predicate

--- FunctionalJava/src/main/java/com/rm/fp/FunctionalJavaApplication.java
+++ FunctionalJava/src/main/java/com/rm/fp/FunctionalJavaApplication.java
@@ -1,13 +1,21 @@
 package com.rm.fp;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.function.Predicate;
 
-@SpringBootApplication
 public class FunctionalJavaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FunctionalJavaApplication.class, args);
+		// SpringApplication.run(FunctionalJavaApplication.class, args);
+		System.out.println("Hello Rishav");
+//		Predicate<String> predicate = x -> x.contains("Hi");
+//		System.out.println(predicate.and(str -> str.contains("Hello")).test("Hi Rishav Hello"));
+
+		testPredicate("Hello", str -> str.contains("Rishav"), x -> x.contains("Hi"));
+	}
+
+	private static void testPredicate(String input, Predicate<String> p1, Predicate<String> p2) {
+		System.out.println(p1.or(Predicate.not(p2)).test(input));
+
 	}
 
 }
