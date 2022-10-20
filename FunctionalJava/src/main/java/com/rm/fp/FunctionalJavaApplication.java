6ef9b02148546cb0f9b0658a12d3659a639be055
rishav.mallick@westpac.com.au <rishav.mallick@westpac.com.au>
2022-10-20 23:53:33 +0530

    first fp project

---
+++ FunctionalJava/src/main/java/com/rm/fp/FunctionalJavaApplication.java
@@ -0,0 +1,13 @@
+package com.rm.fp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionalJavaApplication.class, args);
+	}
+
+}
---
+++ FunctionalJava/src/test/java/com/rm/fp/FunctionalJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rm.fp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
